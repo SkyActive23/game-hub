40a1c00bd0a8e09aa690efee3a6376afef98ffc3
Sky Sky <skyrj50031@gmail.com>
2023-04-29 20:37:12 -0700

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,7 +9,7 @@ export interface Platform{
 }
 
 export interface Game {
-    background_image: string | undefined;
+    background_image: string;
     id: number;
     name: string;
     parent_platforms: {platform: Platform}[]
@@ -23,24 +23,29 @@ export interface Game {
   
 
 const useGames= () => {
-    const [games, setGames] = useState<Game[]>([]);
+  const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false)
 
   useEffect(() => {
 
     const controller = new AbortController();
-
+    setLoading(true)
     apiClient
       .get<FetchGamesResponse>("/games", {signal: controller.signal})
-      .then((res) => setGames(res.data.results))
+      .then((res) => { 
+        setGames(res.data.results)
+        setLoading(false)
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return; 
         setError(err.message)
+        setLoading(false)
     });
     return () => controller.abort();
   }, []);
 
-  return { games, error }
+  return { games, error, isLoading }
 }
 
 export default useGames
